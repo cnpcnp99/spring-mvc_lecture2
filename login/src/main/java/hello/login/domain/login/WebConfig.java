08114193692ca4464b9fc6b6576a22ba654f8fd7
Jeong-EuiJae <47394504+cnpcnp99@users.noreply.github.com>
2022-01-01 03:17:06 +0900

    인터셉터를 통한 보다 나은 방식의 사용자 로그인 체크 처리

--- login/src/main/java/hello/login/domain/login/WebConfig.java
+++ login/src/main/java/hello/login/domain/login/WebConfig.java
@@ -1,17 +1,21 @@
 package hello.login.domain.login;
 
+import hello.login.web.Interceptor.LogInterceptor;
+import hello.login.web.Interceptor.LoginCheckInterceptor;
 import hello.login.web.filter.LogFilter;
 import hello.login.web.filter.LoginCheckFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.Filter;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
+    // @Bean
     public FilterRegistrationBean logFilter() {
         FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
         filterFilterRegistrationBean.setFilter(new LogFilter());
@@ -21,7 +25,7 @@ public class WebConfig {
         return filterFilterRegistrationBean;
     }
 
-    @Bean
+    // @Bean
     public FilterRegistrationBean loginCheckFilter() {
         FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
         filterFilterRegistrationBean.setFilter(new LoginCheckFilter());
@@ -29,5 +33,19 @@ public class WebConfig {
         filterFilterRegistrationBean.addUrlPatterns("/*");
 
         return filterFilterRegistrationBean;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/*.ico", "/error");
+
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(2)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/members/add", "/login",
+                        "/css/**", "/*.ico", "/error");
     }
 }
